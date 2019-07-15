bb2b8cd185308db36515b071ff89280f0cf41561
MornSun <chenkl1995@gmail.com>
2019-07-15 14:10:56 +0800

    update test_numpy_idxs_mask.py

--- python/numpy/test_numpy_idxs_mask.py
+++ python/numpy/test_numpy_idxs_mask.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-lbl = [1, 1, 2, 3, 3, 3, 6, 7, 8, 8, 9]
+lbl = [1, 1, 2, 3, 3, 3, 6, 7, 8, 8, 9, 10]
 # a = np.asarray(a)
 a = np.array(lbl)
 print(a)
@@ -18,7 +18,7 @@ print(unique_idx.dtype, unique_idx.shape, unique_idx)
 c = b[unique_idx]
 print('c =', c)
 
-five_mask = c > 5
+five_mask = c > 6
 print(five_mask.dtype, five_mask.shape, five_mask)
 d = c[five_mask]
 print('d =', d)
@@ -28,5 +28,31 @@ print(eight_mask.dtype, eight_mask.shape, eight_mask)
 e = d[eight_mask]
 print('e =', e)
 
-for i in range(eight_mask.shape[0]):
-    print(i)
+eight_mask_ = np.nonzero(eight_mask)[0]
+print(eight_mask_)
+five_mask_ = np.nonzero(five_mask)[0]
+print(five_mask_)
+
+for i, num in enumerate(e):
+    print(num)
+    print('e idx = ', i)
+
+    d_idx = eight_mask_[i]
+    print('d idx = ', d_idx)
+    assert d[d_idx] == num
+
+    c_idx = five_mask_[d_idx]
+    print('c idx = ', c_idx)
+    assert c[c_idx] == num
+
+    b_idx = unique_idx[c_idx]
+    print('b idx = ', b_idx)
+    assert b[b_idx] == num
+
+    a_idx = sort_idx[b_idx]
+    print('a idx = ', a_idx)
+    assert a[a_idx] == num
+
+    print()
+
+print(sort_idx[unique_idx[five_mask_[eight_mask_[range(len(e))]]]])
